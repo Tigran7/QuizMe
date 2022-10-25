b16b4c7b04045a0466697479cdc7be1be8a6b749
Tigran7 <tarakelov@icloud.com>
2022-10-24 19:57:57 -0700

    test

--- src/main/java/com/example/QuizMe/QuizMeApplication.java
+++ src/main/java/com/example/QuizMe/QuizMeApplication.java
@@ -3,11 +3,20 @@ package com.example.QuizMe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class QuizMeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizMeApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 }
