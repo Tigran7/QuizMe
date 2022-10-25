dc61657cf40d9f9065e5ed6a3d9e598f83b7aee6
Tigran7 <89666148+Tigran7@users.noreply.github.com>
2022-10-24 19:01:42 -0700

    Create QuizMeApplication.java

---
+++ src/main/java/com/example/QuizMe/QuizMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizMeApplication.class, args);
+	}
+
+}
