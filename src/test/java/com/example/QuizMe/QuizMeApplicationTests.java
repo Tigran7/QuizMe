25d313a90ca21d5748601960beb7cb65df39edf7
Tigran7 <89666148+Tigran7@users.noreply.github.com>
2022-10-24 19:06:25 -0700

    Create QuizMeApplicationTests.java

---
+++ src/test/java/com/example/QuizMe/QuizMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuizMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
